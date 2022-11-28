7c571500bd11e91994139bf8b56a7a846262d743
Diana-nyamai <dnyamai.dn@gmail.com>
2022-11-28 14:36:00 +0300

    Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -1,25 +1,31 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Driver {
     public static void main(String[] args) {
         Scanner userinput = new Scanner(System.in);
-        System.out.println("Enter your buying price per share ");
+        System.out.println("Enter your buying price per share: ");
         double buyingprice = userinput.nextDouble();
 
         int day = 1;
         double closingprice = 0.1;
+        DecimalFormat df = new DecimalFormat("0.00");
 
         while(true){
-            System.out.println("Enter closing price for the day");
+            System.out.println("Enter closing price for day " + day + "(any negative values to leave)");
             closingprice = userinput.nextDouble();
             if(closingprice < 0.0) break;
             double earnings = closingprice - buyingprice;
             if(earnings > 0.0){
-                System.out.println("After day " +day+ ", you earned " + earnings + "per share.");
+                System.out.println("After day " +day+ ", you earned " + df.format(earnings) + "  per share.");
             } else if (earnings < 0.0) {
-                System.out.println("After day " +day+ ", you lost " + (-earnings) + "per share.");
-                
+                System.out.println("After day " +day+ ", you lost " + df.format(-earnings) + "per share.");
             }
+            else {
+                System.out.println("After day " + day + " you have " + "no earnings per share" );
+            }
+            day++;
         }
+        userinput.close();
     }
 }
