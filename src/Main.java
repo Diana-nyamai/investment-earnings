f5b5b8f28e8939b4616c3a621533369fac191b7f
Diana-nyamai <dnyamai.dn@gmail.com>
2022-11-28 14:36:27 +0300

    main java

--- src/Main.java
+++ src/Main.java
@@ -3,38 +3,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-//    public String getWinner(){
-//        final String entry;
-//        HashMap<String, Integer> hash = new HashMap<String, Integer>();
-//        hash.put("madza", 100);
-//        hash.put("prado", 2);
-//        hash.put("mercedes", 3);
-//        hash.put("range", 20);
-//        int max = Collections.max(hash.values());
-//
-//
-//        for(Map.Entry<String, Integer> en : hash.entrySet()){
-//            if(en.getValue() == max){
-////                System.out.println(en.getKey())
-//               entry = en.getKey();
-//            }
-//        }
-//        return entry;
-//    }
-    public static int trying(){
-        return 2+7;
-    }
+
     public static void main(String[] args) {
-//        Main win = new Main();
-//        System.out.println(trying());
-//      trying();
-//        win.getWinner();
-//        System.out.println(hash.get("prado"));
-        String [] names = {"diana","wayne","james","victor"};
-        for (int i=0; i<= names.length-1; i++){
-            System.out.println(names[i]);
+        int day = 1;
+        while(day < 10){
+            System.out.println("day: " + day);
+            day++;
         }
-
-
     }
 }
